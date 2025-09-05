ff27d5c6cb04ebb15c6001d91711dce78384f326
Giacomo Bersani <72036069+gbrsni@users.noreply.github.com>
2025-09-05 14:57:04 +0200

    Create /root

--- build_files/10-proxmox-base-packages.sh
+++ build_files/10-proxmox-base-packages.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 
 echo "192.168.1.1 ${$(hostname)}" >> /etc/hosts
 
+mkdir /root
 
 apt-get install -y proxmox-ve postfix open-iscsi chrony
 # apt-get remove -y linux-image-amd64 'linux-image-*' os-prober
